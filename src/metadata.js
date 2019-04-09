c7d910e3299244ee831f9977fe2b316d31b3af52
Arnau Sanchez <pyarnau@gmail.com>
2019-04-09 12:36:47 +0200

    Use shortName for categoryOptionGroups

--- src/metadata.js
+++ src/metadata.js
@@ -12,6 +12,7 @@ const models = [
     "categories",
     { name: "categoryCombos", fields: ["id", "name", "categoryOptionCombos"] },
     "categoryOptions",
+    "categoryOptionGroups",
     "categoryOptionCombos",
 
     "dataElementGroupSets",
@@ -148,12 +149,11 @@ function getCategoryOptionGroupsForAgeGroups(db, antigen, categoryOptionsAgeGrou
     const categoryOptionsAgeGroupsByName = _.keyBy(categoryOptionsAgeGroups, "name");
     const ageGroups = getOrThrow(antigen, "ageGroups");
     const mainAgeGroups = ageGroups.map(group => group[0][0]);
-    const name = getName([antigen.name, "Age groups"]);
 
-    const mainGroup = db.get("categoryOptionGroup", {
-        name: name,
+    const mainGroup = db.get("categoryOptionGroups", {
+        name: getName([antigen.name, "Age groups"]),
         code: `${antigen.code}_AGE_GROUP`,
-        shortName: name,
+        shortName: getName([antigen.shortName || antigen.name, "AG"]),
         categoryOptions: getIds(_.at(categoryOptionsAgeGroupsByName, mainAgeGroups)),
     });
 
@@ -167,10 +167,14 @@ function getCategoryOptionGroupsForAgeGroups(db, antigen, categoryOptionsAgeGrou
         } else {
             return restOfAgeGroup.map((options, index) => {
                 const sIndex = (index + 1).toString();
-                const name = getName([antigen.name, "Age group", mainGroup, sIndex]);
-                return db.get("categoryOptionGroup", {
-                    name: name,
-                    shortName: name,
+                return db.get("categoryOptionGroups", {
+                    name: getName([antigen.name, "Age group", mainGroup, sIndex]),
+                    shortName: getName([
+                        antigen.shortName || antigen.name,
+                        "AGE",
+                        mainGroup,
+                        sIndex,
+                    ]),
                     code: getCode([antigen.code, "AGE_GROUP", mainGroup, sIndex]),
                     categoryOptions: getIds(_.at(categoryOptionsAgeGroupsByName, options)),
                 });
